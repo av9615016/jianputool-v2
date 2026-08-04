f5a7ddc2b2ceaf4bdcfca011595d337aef96dbc5
av9615016 <av9615016@gmail.com>
2026-08-04 13:51:44 +0800

    Update check_measure.py

--- check_measure.py
+++ check_measure.py
@@ -1,13 +1,68 @@
+# check_measure.py
+
+import sys
 from music21 import converter
 
-f = r"C:\Users\user\Desktop\JianpuTool\outputs\c7bf9621\rebuild44_v2.musicxml"
 
-score = converter.parse(f)
+def check_measure(file):
 
-for m in score.parts[0].getElementsByClass('Measure')[:10]:
-    print(
-        "Measure",
-        m.number,
-        "duration=",
-        m.duration.quarterLength
-    )
+    print("讀取:")
+    print(file)
+
+    score = converter.parse(file)
+
+
+    for part in score.parts:
+
+        print("\nPart:", part.id)
+
+
+        for measure in part.getElementsByClass("Measure"):
+
+            total = 0
+
+
+            for n in measure.notesAndRests:
+
+                total += n.duration.quarterLength
+
+
+            print(
+                f"Measure {measure.number}: {total}"
+            )
+
+
+
+if __name__ == "__main__":
+
+
+    if len(sys.argv) < 2:
+
+        print(
+            "使用方式:"
+        )
+
+        print(
+            "python check_measure.py xxx.musicxml"
+        )
+
+        sys.exit(1)
+
+
+    filename = sys.argv[1]
+
+
+    try:
+
+        check_measure(filename)
+
+
+    except Exception as e:
+
+        print(
+            "錯誤:"
+        )
+
+        print(e)
+
+        sys.exit(1)
