4a2d8475408f874e66092ffd276558fb6745ef92
av9615016 <av9615016@gmail.com>
2026-08-05 16:47:07 +0800

    Update check_midi_tracks.py

--- check_midi_tracks.py
+++ check_midi_tracks.py
@@ -1,14 +1,29 @@
 from music21 import converter
-
 import sys
 
 mid = converter.parse(sys.argv[1])
 
-print("MIDI Tracks:")
-
 for i, part in enumerate(mid.parts):
+
+    notes = [
+        n for n in part.recurse().notes
+        if n.isNote
+    ]
+
+    print("Track", i)
+    print("名稱:", part.partName)
+    print("音符數:", len(notes))
+
+    pitches = [n.pitch.midi for n in notes]
+
     print(
-        i,
-        part.partName,
-        len(part.notes)
+        "音域:",
+        min(pitches),
+        "-",
+        max(pitches)
+    )
+
+    print(
+        "前20音:",
+        [str(n.pitch) for n in notes[:20]]
     )
